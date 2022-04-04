bafd22045289adbd9e30505b1dcddc7aa9197313
nacho5036 <99916339+nacho5036@users.noreply.github.com>
2022-04-03 23:58:35 -0500

    Update Problema4.js

--- ProblemasColab/Problema4.js
+++ ProblemasColab/Problema4.js
@@ -4,32 +4,51 @@ function Problema4(){
     var salario=document.getElementById("salario").value; 
 
     if (meses > 1){ 
-        var utilidades1=salario*0.05;
+        var utilidades2=salario*0.05;
         document.getElementById("Utilidades").innerHTML= 
-        '' +utilidades1;   
+        '' +utilidades2;  
     } 
-    else if (meses < 1 meses && > 2){ 
-        var utilidades2=salario*0.07;
+    else if (meses < 1){
+        var utilidades1=salario*0.07;
         document.getElementById("Utilidades").innerHTML= 
-        '' +utilidades2; 
+        '' +utilidades1; 
     }
-
-    else if (meses < 2 meses && > 5){
-        var utilidades3=salario*0.1;
+    else if (meses > 2){
+        var utilidades3=salario*0.07;
         document.getElementById("Utilidades").innerHTML= 
         '' +utilidades3; 
+
+
     }
-   
-    else if (meses < 5 meses && > 10){
-        var utilidades4=salario*0.15;
+    else if (meses < 2){
+        var utilidades4=salario*0.1;
         document.getElementById("Utilidades").innerHTML= 
-        ''+utilidades4;  
+        '' +utilidades4; 
     }
-    
+    else if (meses > 5){
+
+        var utilidades5=salario*0.1;
+        document.getElementById("Utilidades").innerHTML=  
+        ''+utilidades5; 
+
+    }
+    else if (meses < 5){
+        var utilidades6=salario*0.15;
+        document.getElementById("Utilidades").innerHTML= 
+        ''+utilidades6; 
+    }
+    else if (meses > 10){
+        var utilidades7=salario*0.15;
+        document.getElementById("Utilidades").innerHTML= 
+        ''+utilidades7; 
+
+
+
+    }
     else if (meses < 10){ 
-        var utilidades5=salario*0.2;
+        var utilidades8=salario*0.2;
         document.getElementById("Utilidades").innerHTML= 
-        ''+utilidades5;   
+        ''+utilidades8;  
     }
 
 
